5efe69cc00696c9659fee48ff42aeb12936e1fc8
adipawarr <107747999+adipawarr@users.noreply.github.com>
2024-11-24 15:01:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const agentName = {
     Duelist: [
-        { name: "Jett", splashArt: "pngs/jett.webp", voiceLine: "voices/jett-voiceline.mp3" ,agentbg: "agentbg/jett.png"},
-        { name: "Phoenix", splashArt: "pngs/phoenix.webp", voiceLine: "voices/phoenix-voiceline.mp3", agentbg: "agentbg/phoenix.png"},
-        { name: "Reyna", splashArt: "pngs/reyna.webp", voiceLine: "voices/reyna-voiceline.mp3",agentbg: "agentbg/reyna.png" },
-        { name: "Raze", splashArt: "pngs/raze.webp", voiceLine: "voices/raze-voiceline.mp3",agentbg: "agentbg/raze.png" },
-        { name: "Yoru", splashArt: "pngs/yoru.webp", voiceLine: "voices/yoru-voiceline.mp3",agentbg: "agentbg/yoru.png" },
-        { name: "Neon", splashArt: "pngs/neon.webp", voiceLine: "voices/neon-voiceline.mp3",agentbg: "agentbg/neon.png" },
-        { name: "Iso", splashArt: "pngs/iso.webp", voiceLine: "voices/iso-voiceline.mp3",agentbg: "agentbg/iso.png" }
+        { name: "Jett", splashArt: "pngs/jett.webp", voiceLine: "voices/jett-voiceline.mp3" ,agentbg: "agentbg/jett.png",gradient: "linear-gradient(rgb(144, 227, 253), rgb(85, 127, 140), rgb(43, 78, 124), rgb(30, 51,68))"},
+        { name: "Phoenix", splashArt: "pngs/phoenix.webp", voiceLine: "voices/phoenix-voiceline.mp3", agentbg: "agentbg/phoenix.png",gradient: "linear-gradient(rgb(247, 191, 96), rgb(234, 109, 78), rgb(38, 36, 35), rgb(54, 59, 66))"},
+        { name: "Reyna", splashArt: "pngs/reyna.webp", voiceLine: "voices/reyna-voiceline.mp3",agentbg: "agentbg/reyna.png",gradient: "linear-gradient(rgb(228, 110, 119), rgb(196, 77, 159), rgb(47, 38, 100), rgb(47, 32, 63))" },
+        { name: "Raze", splashArt: "pngs/raze.webp", voiceLine: "voices/raze-voiceline.mp3",agentbg: "agentbg/raze.png",gradient: "linear-gradient(rgb(254, 172, 69), rgb(214, 79, 53), rgb(44, 89, 66), rgb(43, 71, 75))" },
+        { name: "Yoru", splashArt: "pngs/yoru.webp", voiceLine: "voices/yoru-voiceline.mp3",agentbg: "agentbg/yoru.png",gradient: "linear-gradient(rgb(102, 168, 255), rgb(59, 55, 167), rgb(38, 30, 79), rgb(16, 16, 66))" },
+        { name: "Neon", splashArt: "pngs/neon.webp", voiceLine: "voices/neon-voiceline.mp3",agentbg: "agentbg/neon.png",gradient: "linear-gradient(rgb(186, 137, 255), rgb(98, 65, 204), rgb(56, 50, 142), rgb(44, 71, 153))" },
+        { name: "Iso", splashArt: "pngs/iso.webp", voiceLine: "voices/iso-voiceline.mp3",agentbg: "agentbg/iso.png",gradient: "linear-gradient(rgb(196, 161, 255), rgb(61, 15, 212), rgb(32, 21, 93), rgb(172, 82, 255))" }
     ],
     Controller: [
-        { name: "Brimstone", splashArt: "pngs/brimstone.webp", voiceLine: "voices/brimstone-voiceline.mp3", agentbg: "agentbg/brimstone.png" },
-        { name: "Viper", splashArt: "pngs/viper.webp", voiceLine: "voices/viper-voiceline.mp3",agentbg: "agentbg/viper.png" },
-        { name: "Omen", splashArt: "pngs/omen.webp", voiceLine: "voices/omen-voiceline.mp3",agentbg: "agentbg/omen.png" },
-        { name: "Clove", splashArt: "pngs/clove.webp", voiceLine: "voices/clove-voiceline.mp3",agentbg: "agentbg/clove.png" },
-        { name: "Astra", splashArt: "pngs/astra.webp", voiceLine: "voices/astra-voiceline.mp3",agentbg: "agentbg/astra.png" },
-        { name: "Harbour", splashArt: "pngs/harbor.webp", voiceLine: "voices/harbour-voiceline.mp3",agentbg: "agentbg/harbor.png" }
+        { name: "Brimstone", splashArt: "pngs/brimstone.webp", voiceLine: "voices/brimstone-voiceline.mp3", agentbg: "agentbg/brimstone.png",gradient: "linear-gradient(rgb(235, 149, 63), rgb(137, 44, 27), rgb(33, 29, 33), rgb(40, 43, 56))" },
+        { name: "Viper", splashArt: "pngs/viper.webp", voiceLine: "voices/viper-voiceline.mp3",agentbg: "agentbg/viper.png",gradient: "linear-gradient(rgb(137, 244, 118), rgb(52, 115, 52), rgb(44, 79, 94), rgb(73, 58, 124))" },
+        { name: "Omen", splashArt: "pngs/omen.webp", voiceLine: "voices/omen-voiceline.mp3",agentbg: "agentbg/omen.png",gradient: "linear-gradient(rgb(47, 221, 222), rgb(79, 83, 175), rgb(52, 70, 115), rgb(25, 59, 61))" },
+        { name: "Clove", splashArt: "pngs/clove.webp", voiceLine: "voices/clove-voiceline.mp3",agentbg: "agentbg/clove.png",gradient: "linear-gradient(rgb(241, 124, 173), rgb(6, 34, 97), rgb(195, 71, 199), rgb(241, 219, 111))" },
+        { name: "Astra", splashArt: "pngs/astra.webp", voiceLine: "voices/astra-voiceline.mp3",agentbg: "agentbg/astra.png",gradient: "linear-gradient(rgb(244, 125, 201), rgb(165, 68, 152), rgb(82, 16, 198), rgb(55, 37, 24))" },
+        { name: "Harbour", splashArt: "pngs/harbor.webp", voiceLine: "voices/harbour-voiceline.mp3",agentbg: "agentbg/harbor.png",gradient: "linear-gradient(rgb(242, 214, 163), rgb(19, 108, 107), rgb(17, 67, 78), rgb(12, 38, 44))" }
 
         
     ],
     Sentinel: [
-        { name: "Sage", splashArt: "pngs/sage.webp", voiceLine: "voices/sage-voiceline.mp3",agentbg: "agentbg/sage.png" },
-        { name: "Cypher", splashArt: "pngs/cypher.webp", voiceLine: "voices/cypher-voiceline.mp3",agentbg: "agentbg/cypher.png" },
-        { name: "Deadlock", splashArt: "pngs/deadlock.webp", voiceLine: "voices/deadlock-voiceline.mp3",agentbg: "agentbg/deadlock.png" },
-        { name: "Killjoy", splashArt: "pngs/killjoy.webp", voiceLine: "voices/killjoy-voiceline.mp3",agentbg: "agentbg/killjoy.png" },
-        { name: "Chamber", splashArt: "pngs/chamber.webp", voiceLine: "voices/chamber-voiceline.mp3",agentbg: "agentbg/chamber.png" },
-        { name: "Vyse", splashArt: "pngs/vyse.webp", voiceLine: "voices/vyse-voiceline.mp3",agentbg: "agentbg/vyse.png" }
+        { name: "Sage", splashArt: "pngs/sage.webp", voiceLine: "voices/sage-voiceline.mp3",agentbg: "agentbg/sage.png",gradient: "linear-gradient(rgb(123, 254, 222), rgb(65, 129, 154), rgb(16, 45, 35), rgb(34, 96, 113))" },
+        { name: "Cypher", splashArt: "pngs/cypher.webp", voiceLine: "voices/cypher-voiceline.mp3",agentbg: "agentbg/cypher.png",gradient: "linear-gradient(rgb(219, 155, 117), rgb(66, 104, 179), rgb(63, 63, 108), rgb(43, 82, 124))" },
+        { name: "Deadlock", splashArt: "pngs/deadlock.webp", voiceLine: "voices/deadlock-voiceline.mp3",agentbg: "agentbg/deadlock.png",gradient: "linear-gradient(rgb(188, 194, 255), rgb(77, 100, 196), rgb(34, 28, 61), rgb(34, 34, 75))" },
+        { name: "Killjoy", splashArt: "pngs/killjoy.webp", voiceLine: "voices/killjoy-voiceline.mp3",agentbg: "agentbg/killjoy.png",gradient: "linear-gradient(rgb(255, 195, 0), rgb(184, 123, 0), rgb(65, 57, 80), rgb(97, 68, 20))" },
+        { name: "Chamber", splashArt: "pngs/chamber.webp", voiceLine: "voices/chamber-voiceline.mp3",agentbg: "agentbg/chamber.png",gradient: "linear-gradient(rgb(255, 206, 111), rgb(123, 77, 33), rgb(55, 45, 43), rgb(22, 53, 85))" },
+        { name: "Vyse", splashArt: "pngs/vyse.webp", voiceLine: "voices/vyse-voiceline.mp3",agentbg: "agentbg/vyse.png",gradient: "linear-gradient(rgb(241, 71, 255), rgb(130, 54, 151), rgb(97, 83, 183), rgb(59, 60, 74))" }
     ],
     Initiator: [
-        { name: "Sova", splashArt: "pngs/sova.webp", voiceLine: "voices/sova-voiceline.mp3",agentbg: "agentbg/sova.png" },
-        { name: "Kayo", splashArt: "pngs/kayo.webp", voiceLine: "voices/kayo-voiceline.mp3",agentbg: "agentbg/kayo.png" },
-        { name: "Fade", splashArt: "pngs/fade.webp", voiceLine: "voices/fade-voiceline.mp3",agentbg: "agentbg/fade.png" },
-        { name: "Gekko", splashArt: "pngs/gekko.webp", voiceLine: "voices/gekko-voiceline.mp3",agentbg: "agentbg/gekko.png" },
-        { name: "Skye", splashArt: "pngs/skye.webp", voiceLine: "voices/skye-voiceline.mp3",agentbg: "agentbg/skye.png" },
-        { name: "Breach", splashArt: "pngs/breach.webp", voiceLine: "voices/breach-voiceline.mp3",agentbg: "agentbg/breach.png" }
+        { name: "Sova", splashArt: "pngs/sova.webp", voiceLine: "voices/sova-voiceline.mp3",agentbg: "agentbg/sova.png",gradient: "linear-gradient(rgb(247, 199, 165), rgb(57, 41, 49), rgb(16, 28, 71), rgb(8, 38, 82))" },
+        { name: "Kayo", splashArt: "pngs/kayo.webp", voiceLine: "voices/kayo-voiceline.mp3",agentbg: "agentbg/kayo.png",gradient: "linear-gradient(rgb(74, 250, 255), rgb(77, 166, 225), rgb(26, 30, 75), rgb(57, 26, 97))" },
+        { name: "Fade", splashArt: "pngs/fade.webp", voiceLine: "voices/fade-voiceline.mp3",agentbg: "agentbg/fade.png",gradient: "linear-gradient(rgb(177, 65, 76), rgb(85, 137, 189), rgb(24, 52, 76), rgb(102, 55, 108))" },
+        { name: "Gekko", splashArt: "pngs/gekko.webp", voiceLine: "voices/gekko-voiceline.mp3",agentbg: "agentbg/gekko.png",gradient: "linear-gradient(rgb(199, 244, 88), rgb(213, 99, 36), rgb(58, 38, 86), rgb(58, 114, 51))" },
+        { name: "Skye", splashArt: "pngs/skye.webp", voiceLine: "voices/skye-voiceline.mp3",agentbg: "agentbg/skye.png",gradient: "linear-gradient(rgb(148, 231, 137), rgb(170, 138, 73), rgb(79, 20, 19), rgb(49, 77, 72))" },
+        { name: "Breach", splashArt: "pngs/breach.webp", voiceLine: "voices/breach-voiceline.mp3",agentbg: "agentbg/breach.png",gradient: "linear-gradient(rgb(255, 156, 51), rgb(176, 70, 33), rgb(82, 58, 35), rgb(68, 65, 46))" }
     ] 
 };
 
@@ -86,6 +86,8 @@ function spinForAgent(selectedType) {
         agentNameDisplay.textContent = randomAgent.name;
         agentSplash.src = randomAgent.splashArt;
         agentNameImage.src = randomAgent.agentbg;;
+
+        slideContainer.style.backgroundImage = randomAgent.gradient;
 
         agentSplash.classList.remove("pop-out");
         agentNameImage.classList.remove("pop-out");
